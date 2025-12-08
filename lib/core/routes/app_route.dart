86a58d6e9f5449270874cace772f4fd82011adf5
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-08 11:44:02 +0400

    Add AppRoute class to define application routes for authentication, login, sign-up, and user profile.

---
+++ lib/core/routes/app_route.dart
@@ -0,0 +1,6 @@
+class AppRoute {
+  static const String authGate = '/';
+  static const String login = '/login';
+  static const String signUp = '/sign-up';
+  static const String profile = '/profile';
+}
