bde9de0d77a7efc512d79f98935537c9760ed541
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-13 15:53:40 +0400

    Refactor main.dart to initialize Firebase, manage dark mode settings, and update app structure to use GetMaterialApp for improved state management and theme handling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:paloma/core/routes/page_router.dart';
+import 'package:paloma/data/repository/auth_repository.dart';
+import 'package:paloma/firebase_options.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Get.put(AuthRepository());
+  final prefs = await SharedPreferences.getInstance();
+  final isDarkMode = prefs.getBool('isDarkMode') ?? false;
+  runApp(MyApp(isDarkMode: isDarkMode));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.isDarkMode});
 
-  // This widget is the root of your application.
+  final bool isDarkMode;
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Paloma',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.mandyRed,
+        useMaterial3: true,
       ),
-      home: const Text("This Is Paloma"),
+
+      darkTheme: FlexThemeData.dark(
+        scheme: FlexScheme.mandyRed,
+        useMaterial3: true,
+      ),
+
+      themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: PageRouter().generateRoute,
     );
   }
 }
