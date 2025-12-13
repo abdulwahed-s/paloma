f104151361c8a905eb927be6721b22a3d24301d6
abdulwahed-s <abdulwahedaldaghir0@gmail.com>
2025-12-13 15:54:00 +0400

    Add HomePage widget to serve as the main screen of the application, integrating various components such as app bar, drawer, posts list, and message input, enhancing overall user experience and layout organization.

---
+++ lib/presentation/screens/home/home_screen.dart
@@ -0,0 +1,61 @@
+// lib/presentation/screens/home/home_screen.dart
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:paloma/controllers/home_controller.dart';
+import 'package:paloma/presentation/widgets/home/home_app_bar.dart';
+import 'package:paloma/presentation/widgets/home/home_drawer.dart';
+import 'package:paloma/presentation/widgets/home/logout_dialog.dart';
+import 'package:paloma/presentation/widgets/home/message_input.dart';
+import 'package:paloma/presentation/widgets/home/posts_empty_state.dart';
+import 'package:paloma/presentation/widgets/home/posts_list.dart';
+import 'package:paloma/presentation/widgets/home/posts_shimmer_loading.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({super.key});
+
+  final HomeController _controller = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+
+    return Scaffold(
+      backgroundColor: colorScheme.background,
+      appBar: HomeAppBar(colorScheme: colorScheme),
+      drawer: HomeDrawer(
+        colorScheme: colorScheme,
+        onShowLogoutDialog: () => LogoutDialog.show(context),
+      ),
+      body: Column(
+        children: [
+          // Add a subtle divider
+          Container(
+            height: 1,
+            color: colorScheme.outlineVariant.withOpacity(0.3),
+          ),
+          Expanded(
+            child: Obx(() {
+              if (_controller.isLoading) {
+                return PostsShimmerLoading(colorScheme: colorScheme);
+              }
+              if (_controller.posts.isEmpty) {
+                return PostsEmptyState(colorScheme: colorScheme);
+              }
+
+              return PostsList(
+                posts: _controller.posts,
+                users: _controller.users,
+              );
+            }),
+          ),
+          MessageInput(
+            colorScheme: colorScheme,
+            controller: _controller.messageController,
+            onSendPost: _controller.sendPost,
+          ),
+        ],
+      ),
+    );
+  }
+}
